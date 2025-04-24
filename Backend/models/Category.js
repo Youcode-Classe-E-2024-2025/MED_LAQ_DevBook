4ae392d874617e33985eb0e80866b42199a4655c
medlaq777 <m.laqsoumi@gmail.com>
2025-04-24 09:16:06 +0100

    create the class Category.js

---
+++ Backend/models/Category.js
@@ -0,0 +1,8 @@
+class Category {
+    constructor(id, name, description, createdAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+}
