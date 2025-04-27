c37250713a2a5b7798e72de880b90fea2591390e
medlaq777 <m.laqsoumi@gmail.com>
2025-04-27 22:24:59 +0100

    update the front end and add borrow btn

--- Frontend/js/fetchBooks.js
+++ Frontend/js/fetchBooks.js
@@ -21,20 +21,27 @@ document.addEventListener("DOMContentLoaded", function () {
         emptyMessage.style.display = "none";
         books.forEach((book) => {
           const row = document.createElement("tr");
+            const publishedDate = new Date(book.published_date);
+            const day = String(publishedDate.getDate()).padStart(2, '0');
+            const month = String(publishedDate.getMonth() + 1).padStart(2, '0');
+            const year = String(publishedDate.getFullYear());
             row.innerHTML = `
-            <td class="px-6 py-4">${book.title}</td>
-            <td class="px-6 py-4">${book.author}</td>
-            <td class="px-6 py-4">${book.category_name}</td>
-            <td class="px-6 py-4">${book.published_date}</td>
-            <td class="px-6 py-4">${book.isbn}</td>
-            <td class="px-6 py-4">
+              <td class="px-6 py-4">${book.title}</td>
+              <td class="px-6 py-4">${book.author}</td>
+              <td class="px-6 py-4">${book.category_name}</td>
+              <td class="px-6 py-4">${day}-${month}-${year}</td>
+              <td class="px-6 py-4">${book.isbn}</td>
+              <td class="px-6 py-4">
               <button class="edit-btn bg-blue-500 text-white px-2 py-1 rounded mr-2" data-id="${book.id}">
-              <i class="fas fa-edit"></i>
+                <i class="fas fa-edit"></i>
               </button>
-              <button class="delete-btn bg-red-500 text-white px-2 py-1 rounded" data-id="${book.id}">
-              <i class="fas fa-trash-alt"></i>
+              <button class="delete-btn bg-red-500 text-white px-2 py-1 rounded mr-2" data-id="${book.id}">
+                <i class="fas fa-trash-alt"></i>
               </button>
-            </td>
+              <button class="borrow-btn bg-green-500 text-white px-2 py-1 rounded" data-id="${book.id}">
+                <i class="fas fa-book-reader"></i>
+              </button>
+              </td>
             `;
           booksList.appendChild(row);
         });
