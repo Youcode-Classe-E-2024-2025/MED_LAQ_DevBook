957409cd9540bc778270908891cc0b94bb1aee37
medlaq777 <m.laqsoumi@gmail.com>
2025-04-27 21:40:21 +0100

    fetch all books and display data on the home page

--- Backend/Repository/BookRepository.js
+++ Backend/Repository/BookRepository.js
@@ -5,7 +5,7 @@ class BookRepository {
 
   findAll() {
     return new Promise((resolve, reject) => {
-      const query = "SELECT * FROM books";
+      const query = "SELECT * , c.name AS category_name FROM books b JOIN categories c ON b.category_id = c.id";
       this.db.query(query, (err, results) => {
         resolve(results);
       });
